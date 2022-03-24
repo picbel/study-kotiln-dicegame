ec18634c05a13fb73b6b1fe021ec076f05fd51de
Picbel <picbel.gon@gmail.com>
2022-03-24 19:16:58 +0900

    Dice를 도메인화 해보기 연산자 오버라이딩 해보기

--- src/test/kotlin/app/domain/DiceTest.kt
+++ src/test/kotlin/app/domain/DiceTest.kt
@@ -13,7 +13,7 @@ internal class DiceTest{
     }
 
     @ParameterizedTest
-    @ValueSource(ints = [0, 2, 4])
+    @ValueSource(ints = [2, 4, 6])
     fun `주사위값이 짝수라면 거짓을 반환한다`(num: Int) {
         assertFalse(Dice(6,num).isOdd())
     }
